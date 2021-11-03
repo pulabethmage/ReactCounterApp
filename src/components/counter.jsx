163dec17d3c478716cddde252c42b25e0c5e406a
Pulasthi Bethmage <87483405+pulabethmage@users.noreply.github.com>
2021-11-03 06:52:27 +0530

    Event Handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,19 @@ import React, { Component } from "react";
 class Counter extends React.Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
     // imageUrl: "https://picsum.photos/200",
   };
+
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this);
+  }
+
+  handleIncrement(product) {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  }
 
   //   style = {
   //     fontSize: 10,
@@ -15,10 +26,21 @@ class Counter extends React.Component {
     return (
       <div>
         {/* <img src={this.state.imageUrl} alt="" /> */}
-        <span className="badge bg-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <span className={this.getCounterClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </div>
     );
+  }
+
+  getCounterClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
